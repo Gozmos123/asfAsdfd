a3a0ae3fd1b3011e0a4ccc18aa6a062a95b1dce5
Joshua Villafuerte <joshuar.villafuerte9@gmail.com>
2023-02-27 09:36:40 +0800

    added activity logs
    modified activity log

---
+++ bhis/modals/modal_view_log.php
@@ -0,0 +1,58 @@
+<div class="modal fade" id="modal_view_log" tabindex="-1" role="dialog" aria-labelledby="modalViewLog" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalViewLog"><strong>Log Details</strong></h5>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <p class="mb-0">Username</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <p class="text-muted mb-0" id="username"></p>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <p class="mb-0">Action</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <p class="text-muted mb-0" id="action"></p>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <p class="mb-0">Content</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <p class="text-muted mb-0" id="log_content"></p>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <p class="mb-0">Changes</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <p class="text-muted mb-0" id="log_changes"></p>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <p class="mb-0">Date</p>
+                    </div>
+                    <div class="col-sm-9">
+                        <p class="text-muted mb-0" id="date"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" id="btn_close" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
