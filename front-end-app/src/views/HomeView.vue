c4c6e0fe4fa7df222113f7d321e32dac4201fa5e
Joshua R. Villafuerte <joshuar.villafuerte9@gmail.com>
2024-08-28 21:26:45 +0800

    added fresh vue scaffolding

---
+++ front-end-app/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const counter = useCounterStore();
+</script>
+
+<template>
+    <main>
+        <h1>Home Page</h1>
+        <button @click="counter.increment()">Click Counter Add</button>
+        <div>Current Count: {{ counter.count }}</div>
+        <div>Current Double Count: {{ counter.doubleCount }}</div>
+    </main>
+</template>
